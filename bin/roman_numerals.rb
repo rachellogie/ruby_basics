75b0eafb94b64a99256c91a424cafc59e2afe763
rachellogie <rachel.logie1@gmail.com>
2014-03-08 12:14:14 -0700

    passed all tests for roman numerals

--- bin/roman_numerals.rb
+++ bin/roman_numerals.rb
@@ -2,23 +2,58 @@
 
 number = gets.chomp.to_i
 
-puts "number is #{number}"
+thousands = number / 1000
+number = number % 1000
 
-=begin
-1 ... I
-2 ... II
-3 ... III
-4 ... IV
-5 ... V
-6 ... VI
-7 ... VII
-8 ... VIII
-9 ... IX
+nine_hundreds = number / 900
+number = number % 900
 
-=end
+five_hundreds = number / 500
+number = number % 500
+
+four_hundreds = number / 400
+number = number % 400
+
+hundreds = number / 100
+number = number % 100
+
+nineties = number / 90
+number = number % 90
+
+fifties = number / 50
+number = number % 50
+
+forties = number / 40
+number = number % 40
+
+tens = number / 10
+number = number % 10
+
+nines = number / 9
+number = number % 9
 
 fives = number / 5
-ones = number % 5
+number = number % 5
 
-puts "V" * fives + "I" * ones
+fours = number / 4
+number = number % 4
+
+ones = number
+
+
+
+puts "M" * thousands +
+       "CM" * nine_hundreds +
+       "D" * five_hundreds +
+       "CD" * four_hundreds +
+       "C" * hundreds +
+       "IX" * nineties +
+       "L" * fifties +
+       "XL" * forties +
+       "X" * tens +
+       "IX" * nines +
+       "V" * fives +
+       "IV" * fours +
+       "I" * ones
+
 
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -10,7 +10,6 @@ describe "RomanNumerals" do
   end
 
   it "handles the numbers that are just below the next boundary" do
-    pending
     run_script("roman_numerals.rb")
     type("9")
 
@@ -19,7 +18,6 @@ describe "RomanNumerals" do
   end
 
   it "converts a double digit number to traditional roman numeral" do
-    pending
     run_script("roman_numerals.rb")
     type("39")
 
@@ -28,7 +26,6 @@ describe "RomanNumerals" do
   end
 
   it "converts the triple digit number to traditional roman numeral" do
-    pending
     run_script("roman_numerals.rb")
     type("257")
 
@@ -37,7 +34,6 @@ describe "RomanNumerals" do
   end
 
   it "converts the four-digit number to traditional roman numerals" do
-    pending
     run_script("roman_numerals.rb")
     type("1468")
 
